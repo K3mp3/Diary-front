f7f689c2da729b22187968949f42282007ffd9c0
Melvin Kempe <k3mp314@gmail.com>
2023-03-17 17:53:09 +0100

    [added] styling to login form

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ let loginPassword = document.querySelector(".loginPassword");
 let loginUserBtn = document.querySelector(".loginUserBtn");
 let userGreeting = document.querySelector(".userGreeting");
 
+function createLoginDesign() {
+    const loginContainer = document.createElement("div");
+    const blurBackgroundDiv = document.createElement("div");
+    const loginFormContainer = document.createElement("div");
+    const loginBg = document.createElement("div");
+    const loginForm = document.createElement("div");
+    const loginHeading = document.createElement("h1");
+    
+    loginContainer.classList.add("login-container");
+    blurBackgroundDiv.classList.add("blur-background-div")
+    loginFormContainer.classList.add("login-form-container");
+    loginBg.classList.add("login-bg");
+    loginForm.classList.add("login-form");
+    loginHeading.classList.add("login-heading");
+
+    loginHeading.innerHTML= "Sign in";
+
+    document.body.appendChild(loginContainer)
+    loginContainer.appendChild(blurBackgroundDiv)
+    loginContainer.appendChild(loginFormContainer);
+    loginFormContainer.appendChild(loginBg);
+    loginFormContainer.appendChild(loginForm);
+    loginForm.appendChild(loginHeading);
+}
+
+createLoginDesign();
+
 fetch("http://localhost:3000/users")
 .then(res => res.json())
 .then(data => {
